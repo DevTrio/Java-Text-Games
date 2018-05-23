00ea17d08ecfe71be486df6451f82903330d76a5
Joshua Merrill <nw064-git@maskr.me>
2018-05-23 12:20:52 -0700

    Update and rename legendaryText.java to LegendaryText.java

--- java/LegendaryText.java
+++ java/LegendaryText.java
@@ -1,4 +1,4 @@
-public class HelloWorld {
+public class LegendaryText {
   public static void main(String[] args) {
     System.out.println("LEGENDARY TEXT");
     System.out.println("Press [ENTER]");
