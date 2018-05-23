a8782b96be1ea5b9e79b2726c3eb9f90763c47c7
Joshua Merrill <nw064-git@maskr.me>
2018-05-23 14:08:25 -0700

    Delete PyAdventJavaEd.java

--- java/pythonadventure/PyAdventJavaEd.java
+++
@@ -1,7 +0,0 @@
-public class PyAdventJavaEd {
-  public static void main(String[] args) {
-    System.out.println("PYTHON ADVENTURE");
-    System.out.println("Java Edition");
-    System.out.println("Press [ENTER]");
-  }
-}
