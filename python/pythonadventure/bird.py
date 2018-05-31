9d1e1ad1d459a14f26f2134f2bb12502eff7ebb2
Jacob Merrill <jrmerrill2004@gmail.com>
2018-05-31 13:18:08 -0700

    Update bird.py

--- python/pythonadventure/bird.py
+++ python/pythonadventure/bird.py
@@ -1,1 +1,6 @@
-
+# the bird statue subprogram. Runs when the user activates a bird statue.
+strings = ["Save", "Map", "Warp", "Save and Quit"]
+# files:
+currSaveFile = open("./currentSave.txt", "r")
+currSave = currSaveFile.readline()
+locFile = open("./data/" + currsave + "/location.txt", "r+")
